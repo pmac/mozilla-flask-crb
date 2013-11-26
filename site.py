6744a1a0f3990817f0607cfe17575a0a52ee0fc0
David Ray <davidray@gmail.com>
2013-11-25 21:37:04 -0500

    add default logo (wrong final size); add routes and template in prep for customization

--- site.py
+++ site.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/about/")
+def about():
+    return render_template('about.html')
+
+@app.route("/why_crb/")
+def why_crb():
+    return render_template('why_crb.html')
+
+@app.route("/members/")
+def members():
+    return render_template('members.html')
+
+@app.route("/certified-products/")
+def certified_products():
+    return render_template('certified_products.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run(port=8000)
